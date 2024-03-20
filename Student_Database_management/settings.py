1bb73cc6d74561eef17e41e2b8a84c58dfa5537d
Gokul-N07 <gokul2k2002@gmail.com>
2024-03-20 19:53:40 +0530

    New file added

--- Student_Database_management/settings.py
+++ Student_Database_management/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR2,'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
